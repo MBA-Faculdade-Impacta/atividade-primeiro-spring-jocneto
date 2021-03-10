d4383c3bf72d96150e278fbac81df8d57e1aae11
jocneto <jocneto20@yahoo.com.br>
2021-03-10 00:25:49 +0000

    tentativa 1

---
+++ src/main/java/br/com/impacta/lab/controller/Produto.java
@@ -0,0 +1,34 @@
+package br.com.impacta.lab.controller;
+
+public class Produto {
+
+	private int codigo;
+	private String descricao;
+	private double tipo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int codigo) {
+		this.codigo = codigo;
+	}
+
+	  public String descricao() {
+		return descricao;
+	}
+
+	public void setdescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public double tipo() {
+		return tipo;
+	}
+
+	public void tipo(double tipo) {
+		this.tipo = tipo;
+	}
+
+}
